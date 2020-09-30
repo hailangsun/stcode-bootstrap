d5efe9874eb849fbbf275d004d55218f29ee944c
hailangsun <427909039@163.com>
2020-09-30 13:19:39 +0800

    20200930align

--- src/main/java/com/stcode/bootstrap/model/Grjgylzf.java
+++ src/main/java/com/stcode/bootstrap/model/Grjgylzf.java
@@ -32,15 +32,51 @@ public class Grjgylzf extends Jcjg{
     private String jbr;
     //补支金额大于
     private String bzjgdy;
-
-
+    //检查类型
+    private String jclx;
+    //审核状态
+    private String shbz;
 
 
     /**
-     * 一级页面
+     * 一级页面 返回结果
      */
+    //单位名称
+    private String dwmc;
+    //报表日期
+    private String bbrq;
+    //补支原因
+    private String bzyy;
+    //经（代）办机构
+    private String zfqx;
+    //办理日期
+    private String blrq;
 
-
+    /**
+     * 点击名称 二级页面
+     */
+    //支付金额
+    private String zfje;
+    //基础养老金
+    private String zfjc;
+    //个人账户
+    private String zfzh1;
+    //过渡养老金
+    private String zfgd;
+    //基础补贴
+    private String ylj3;
+    //建国前补贴
+    private String ylj4;
+    //调整机制
+    private String ylj5;
+    //护理费
+    private String hlf;
+    //统筹负担
+    private String tcfd;
+    //发放方式 (dmm=0290)
+    private String fffs;
+    //发放地点(dmm=0233)
+    private String ffdd;
 
     /**
      * 检查结果jcjg
